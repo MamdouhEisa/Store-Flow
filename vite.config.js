8ed423d79610c2de5c992cf19b13ab771201c570
unknown <https://github.com/MamdouhEisa/fresh-mart.git>
2026-05-11 15:21:21 +0300

    fix vercel path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-const repoName = "Store-Flow";
+const repoName = "/";
 
 export default defineConfig({
   base: process.env.NODE_ENV === "production" ? `/${repoName}/` : "/",
